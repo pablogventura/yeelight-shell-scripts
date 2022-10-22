9a6eec782fcb98548666f87e2eaf6c931748ac86
Pablo Ventura <pablogventura@gmail.com>
2022-10-22 18:40:47 -0300

    mis preferencias

--- yeelight-redshift.sh
+++ yeelight-redshift.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 redshift -p &> redshift_out
-string=$(cat redshift_out | grep Color)
+string=$(cat redshift_out | grep color)
 temp=$(echo $string | tr -d -c 0-9)
 bright=$(python -c "print(round($temp*0.02-30))")
-temp=$(python -c "print(round($temp*1.75-4875))")  #adjust constants for your preference
+echo $temp
+temp=$(python -c "print(min((max((round($temp-500)),4000)),5900))")  #adjust constants for your preference
+echo $temp
 $( dirname $0 )/yeelight-colortemp.sh 0 $temp
-$( dirname $0 )/yeelight-brightness.sh 0 $bright
+$( dirname $0 )/yeelight-brightness.sh 0 100
