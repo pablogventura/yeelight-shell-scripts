fb42eee24c6aa9a4eba3cd59dcf5e9ce977635bd
Pablo Ventura <pablogventura@gmail.com>
2022-10-22 18:44:17 -0300

    comentario

--- yeelight-redshift.sh
+++ yeelight-redshift.sh
@@ -4,8 +4,8 @@ redshift -p &> redshift_out
 string=$(cat redshift_out | grep color)
 temp=$(echo $string | tr -d -c 0-9)
 bright=$(python -c "print(round($temp*0.02-30))")
-echo $temp
+#echo $temp
 temp=$(python -c "print(min((max((round($temp-500)),4000)),5900))")  #adjust constants for your preference
-echo $temp
+#echo $temp
 $( dirname $0 )/yeelight-colortemp.sh 0 $temp
 $( dirname $0 )/yeelight-brightness.sh 0 100
